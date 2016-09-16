ebc9fc8524ac376103159574e0444acee6abb854
Andreas Malling Østergaard <andreas.malling@gmail.com>
2016-09-16 10:30:36 +0200

    Update entry-point to allow shell access if no args is passed

--- entry-point.sh
+++ entry-point.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-cd /root/cave
-ant $@
+# Find path for entry-point.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# If no args is provided, default to shell
+# else run build.xml with provided commands
+if [ $# -eq 0 ]; then
+  echo "No args provided, starting shell."
+  exec /bin/bash
+else
+  cd $DIR
+  ant $@
+fi
