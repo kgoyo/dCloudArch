d0b3cd1040674728c05362467e70fd46720657b1
Andreas Malling Østergaard <andreas.malling@gmail.com>
2016-09-22 16:37:16 +0200

    more mongo

---
+++ test/cloud/cave/service/TestMongoDBStorage.java
@@ -0,0 +1,14 @@
+package cloud.cave.service;
+
+import org.junit.Before;
+
+/**
+ * Created by amao on 9/22/16.
+ */
+public class TestMongoDBStorage {
+
+    @Before
+    public void setup{
+        MongoClient mongoClient = new MongoClient();
+    }
+}
